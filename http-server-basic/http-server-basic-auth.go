c8aec2f870d2c010728393b1085bd466cf1f58e2
Leo <leogutierrezramirez@gmail.com>
2018-12-31 20:15:15 -0700

    adding project to test compression

--- http-server-basic/http-server-basic-auth.go
+++ http-server-basic/http-server-basic-auth.go
@@ -22,7 +22,8 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 func BasicAuth(handler http.HandlerFunc, realm string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		user, pass, ok := r.BasicAuth()
-		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(ADMIN_USER)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(ADMIN_PASSWORD)) != 1 {
+		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(ADMIN_USER)) != 1 ||
+			subtle.ConstantTimeCompare([]byte(pass), []byte(ADMIN_PASSWORD)) != 1 {
 			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
 			w.WriteHeader(401)
 			w.Write([]byte("You are Unauthorized to access the application.\n"))
