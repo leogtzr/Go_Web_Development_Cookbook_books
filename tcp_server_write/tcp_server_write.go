a7b275a94127662510d6e9aa3a4ed98c3ed612ec
Leo <leogutierrezramirez@gmail.com>
2018-12-31 21:43:24 -0700

    writing to tcp server.

--- tcp_server_write/tcp_server_write.go
+++ tcp_server_write/tcp_server_write.go
@@ -10,16 +10,14 @@ import (
 const (
 	CONN_HOST = "localhost"
 	CONN_PORT = "8080"
-	// CONN_TYPE represents ...
 	CONN_TYPE = "tcp"
 )
 
 func main() {
 	listener, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 	if err != nil {
-		log.Fatal("Error starting tcp server: ", err)
+		log.Fatal("Error starting tcp server:", err)
 	}
-
 	defer listener.Close()
 
 	log.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
@@ -39,6 +37,7 @@ func handleRequest(conn net.Conn) {
 		fmt.Println("Error reading: ", err.Error())
 	}
 
-	fmt.Println("message received from the client:", string(message))
+	fmt.Println("Message received: ", string(message))
+	conn.Write([]byte(message + "\n"))
 	conn.Close()
 }
