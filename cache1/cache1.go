64534caedd56835dd013d070a52317d9edeb54c6
leogtzr <leogutierrezramirez@gmail.com>
2019-01-03 13:04:16 -0700

    implementing little cache

---
+++ cache1/cache1.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/patrickmn/go-cache"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "8080"
+)
+
+var newCache *cache.Cache
+
+func init() {
+	newCache = cache.New(5*time.Minute, 10*time.Minute)
+	newCache.Set("foo", "bar", cache.DefaultExpiration)
+}
+
+func getFromCache(w http.ResponseWriter, r *http.Request) {
+	foo, found := newCache.Get("foo")
+	if found {
+		log.Print("key found in Cache with value as :: ", foo.(string))
+		fmt.Fprintf(w, "Hello "+foo.(string))
+	} else {
+		log.Print("key not found in Cache :: ", "foo")
+		fmt.Fprintf(w, "key not found in cache")
+	}
+}
+
+func main() {
+	http.HandleFunc("/", getFromCache)
+	err := http.ListenAndServe(CONN_HOST+":"+CONN_PORT, nil)
+	if err != nil {
+		log.Fatal("error starting http server: ", err)
+		return
+	}
+}
