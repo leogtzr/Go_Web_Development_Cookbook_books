97f46f07bb6b1df265ed2ae8aee6ea9b1055d850
leogtzr <leogutierrezramirez@gmail.com>
2019-01-04 14:44:18 -0700

    reactJS app

---
+++ reactjs-client/app/main.js
@@ -0,0 +1,9 @@
+'use strict';
+const React = require('react');
+const ReactDOM = require('react-dom')
+import EmployeeApp from '../components/react-app'
+ReactDOM.render
+(
+  <EmployeeApp />,
+  document.getElementById('react')
+)
